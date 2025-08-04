10b6b668cb90a71be74688ae3435ec3e1d81e819
wadi <zabir4897@gmail.com>
2025-08-04 17:50:58 +0600

    solve some error

--- database/migrations/2025_07_22_114338_create_products_table.php
+++ database/migrations/2025_07_22_114338_create_products_table.php
@@ -1,42 +1,4 @@
 <?php
-//
-//use Illuminate\Database\Migrations\Migration;
-//use Illuminate\Database\Schema\Blueprint;
-//use Illuminate\Support\Facades\Schema;
-//
-//return new class extends Migration
-//{
-//    /**
-//     * Run the migrations.
-//     */
-//    public function up()
-//    {
-//        Schema::create('products', function (Blueprint $table) {
-//            $table->id();
-//            $table->string('name');
-//            $table->text('description')->nullable();
-//            $table->decimal('price', 10, 2);
-//            $table->unsignedBigInteger('category_id');
-//            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-//            $table->integer('stock')->default(0);
-//            $table->string('size'); // S, M, L, XL
-//            $table->string('team'); // Brazil, Argentina etc
-//            $table->string('color'); // Yellow, Blue etc
-//            $table->enum('variant', ['home', 'away', 'special', 'other']);
-//            $table->string('image')->nullable(); // image path
-//            $table->timestamps();
-//        });
-//    }
-//
-//    /**
-//     * Reverse the migrations.
-//     */
-//    public function down(): void
-//    {
-//        Schema::dropIfExists('products');
-//    }
-//};
-
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
